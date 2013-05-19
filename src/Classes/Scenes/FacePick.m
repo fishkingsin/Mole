28ee1197bda99f395c1cd17e50df8db2b71e8fd4
fishkingsin <fishkingsin@gmail.com>
2013-05-20 00:22:45 +0800

    added buttton in FacePick

--- src/Classes/Scenes/FacePick.m
+++ src/Classes/Scenes/FacePick.m
@@ -10,15 +10,20 @@
 @interface FacePick ()
 - (void)onImageTouched:(SPTouchEvent *)event;
 - (void)onButtonTriggered:(SPEvent *)event;
-
+- (void)onMaleTriggered:(SPEvent *)event;
+- (void)onFemaleTriggered:(SPEvent *)event;
+-(SPButton*) createButton:(NSString*) _text : (NSString*)filePath;
 @end
 @implementation FacePick
 {
 //    SPSprite *self;
-    NSArray *_thumbnailImages;
+    NSArray *_femaleThumbnailImages;
+    NSArray *_maleThumbnailImages;
     float _offsetY;
         NSString *_faceFile;
     SPButton *conifirmButton;
+    SPButton *maleButton;
+    SPButton *femaleButton;
 
 }
 @synthesize faceFile = _faceFile;
@@ -46,15 +51,50 @@
     NSString *name = [defaults objectForKey:@"UserName"];
     SPTextField * _userNameTF = [SPTextField textFieldWithWidth:100 height:25
                                                      text:name];
-    _userNameTF.x = 20;
-    _userNameTF.y = 20;
+    _userNameTF.x = 10;
+    _userNameTF.y = 10;
     _userNameTF.hAlign = SPHAlignLeft;
     _userNameTF.vAlign = SPVAlignTop;
     _userNameTF.border = NO;
     _userNameTF.color = 0x000000;
     [self addChild:_userNameTF];
     
-    _thumbnailImages = [NSArray arrayWithObjects:
+    
+    maleButton = [self createButton:@"Male" :@"button_short.png"];
+    maleButton.x = (Sparrow.stage.width-( maleButton.width*2))*0.5;
+    maleButton.y = 0;
+    maleButton.enabled = NO;
+    [maleButton addEventListener:@selector(onMaleTriggered:) atObject:self
+                        forType:SP_EVENT_TYPE_TRIGGERED];
+    [self addChild:maleButton];
+    
+    femaleButton = [self createButton:@"Female" :@"button_short.png" ];
+    femaleButton.x = (Sparrow.stage.width-( maleButton.width*2))*0.5+maleButton.width;
+    femaleButton.y = 0;
+    femaleButton.enabled = YES;
+    [femaleButton addEventListener:@selector(onFemaleTriggered:) atObject:self
+                         forType:SP_EVENT_TYPE_TRIGGERED];
+    [self addChild:femaleButton];
+    
+//    [maleButton addEventListener:@selector(onMaleTriggered::) atObject:self
+//                        forType:SP_EVENT_TYPE_TRIGGERED];
+    _maleThumbnailImages = [NSArray arrayWithObjects:
+                              @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            @"ks_holy-tricky_male.png",
+                            [SPTexture textureWithContentsOfFile:@"ks_holy-tricky_male_thumb.png"],
+                            nil];
+
+    
+    _femaleThumbnailImages = [NSArray arrayWithObjects:
                        @"tse_holy-tricky_female.png",
                        [SPTexture textureWithContentsOfFile:@"tse_holy-tricky_female_thumb.png"],
                        @"ks_holy-tricky_female.png",
@@ -76,16 +116,17 @@
                        nil];
     int index = 0;
     int count = 0;
-
-    while (index < _thumbnailImages.count)
+    float offSetX = (Sparrow.stage.width-315)*0.5;
+    float offSetY = (Sparrow.stage.height-315)*0.5;
+    while (index < _maleThumbnailImages.count)
     {
-        NSString *faceName = _thumbnailImages[index++];
-        SPTexture * image = _thumbnailImages[index++];
+        NSString *faceName = _maleThumbnailImages[index++];
+        SPTexture * image = _maleThumbnailImages[index++];
 
         
         SPButton *button = [SPButton buttonWithUpState: image];
-        button.x = (count % 3)*105;
-        button.y = (count / 3) * 105;
+        button.x = offSetX+(count % 3)*105;
+        button.y = offSetY+(count / 3) * 105;
         button.name = faceName;
         
         
@@ -123,5 +164,36 @@
 - (NSString*) getTargetFace
 {
     return _faceFile;
+}
+
+- (void)onMaleTriggered:(SPEvent *)event
+{
+    maleButton.enabled = NO;
+    femaleButton.enabled = YES;
+}
+- (void)onFemaleTriggered:(SPEvent *)event
+{
+    maleButton.enabled = YES;
+    femaleButton.enabled = NO;
+}
+-(SPButton*) createButton:(NSString*) _text : (NSString*)filePath
+{
+    SPTexture *buttonTexture = [SPTexture textureWithContentsOfFile:filePath];
+    
+    SPButton *newButton = [[SPButton alloc] initWithUpState:buttonTexture text:_text];
+    
+    newButton.name = _text;
+    newButton.enabled = YES;
+    
+
+    return newButton;
+}
+- (void)onSceneClosing:(SPEvent *)event
+{
+    
+    [femaleButton removeEventListenersAtObject:self forType:SP_EVENT_TYPE_TRIGGERED];
+    [maleButton removeEventListenersAtObject:self forType:SP_EVENT_TYPE_TRIGGERED];
+    
+     [super onSceneClosing:event];
 }
 @end
