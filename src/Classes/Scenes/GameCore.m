7d340d3d8a4d32a5363c5d61ddab6d45e9dc7452
James Kong <fishkingsin@gmail.com>
2013-05-20 17:57:48 +0800

    remove checkplatform function (not complete)

--- src/Classes/Scenes/GameCore.m
+++ src/Classes/Scenes/GameCore.m
@@ -122,27 +122,29 @@ void releaseData(void *info, const void *data, size_t dataSize) {
     
 	int myWidth = 640;
 	int myHeight = 960;
-    if ([[self platformString] isEqualToString:@"iPhone 3G"] ||
-        [[self platformString] isEqualToString:@@"iPhone 3GS"])
-    {
-        myWidth = 320;
-        myHeight = 480;
-    }
-    else if ([[self platformString] isEqualToString:@"iPhone 4"] ||
-        [[self platformString] isEqualToString:@"Verizon iPhone 4"] ||
-        [[self platformString] isEqualToString:@"iPhone 4S"])
-    {
+    int myX = 0;
+    int myY = 0;
+//    if ([[self platformString] isEqualToString:@"iPhone 3G"] ||
+//        [[self platformString] isEqualToString:@"iPhone 3GS"])
+//    {
+//        myWidth = 320;
+//        myHeight = 480;
+//    }
+//    else if ([[self platformString] isEqualToString:@"iPhone 4"] ||
+//        [[self platformString] isEqualToString:@"Verizon iPhone 4"] ||
+//        [[self platformString] isEqualToString:@"iPhone 4S"])
+//    {
         myWidth = 640;
         myHeight = 960;
-    }
-    else if ([[self platformString] isEqualToString:@"iPhone 5 (GSM)"] ||
-        [[self platformString] isEqualToString:@"iPhone 5 (GSM+CDMA)"])
-    {
-        myWidth = 640;
-        myHeight = 960;
-        myX = 0;
-        myY = (1136-960)/2;
-    }
+//    }
+//    else if ([[self platformString] isEqualToString:@"iPhone 5 (GSM)"] ||
+//        [[self platformString] isEqualToString:@"iPhone 5 (GSM+CDMA)"])
+//    {
+//        myWidth = 640;
+//        myHeight = 960;
+//        myX = 0;
+//        myY = (1136-960)/2;
+//    }
 
     
     NSInteger myDataLength = myWidth * myHeight * 4;
@@ -161,6 +163,9 @@ void releaseData(void *info, const void *data, size_t dataSize) {
 	CGContextDrawImage(context, CGRectMake(0.0, 0.0, myWidth, myHeight), iref);
 	CGImageRef outputRef = CGBitmapContextCreateImage(context);
 	UIImage *image = [UIImage imageWithCGImage:outputRef];
+    CGImageRelease(outputRef);
+    CGImageRelease(iref);
+    CGContextRelease(context);
 	free(pixels);
 	return image;
     //    [image release];
@@ -287,37 +292,37 @@ void releaseData(void *info, const void *data, size_t dataSize) {
     }
        [super render:support];
 }
-- (NSString *) platformString{
-    NSString *platform = [self platform];
-    if ([platform isEqualToString:@"iPhone1,1"])    return @"iPhone 1G";
-    if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
-    if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
-    if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
-    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
-    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
-    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
-    if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
-    if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
-    if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
-    if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
-    if ([platform isEqualToString:@"iPad1,1"])      return @"iPad";
-    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
-    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
-    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini (WiFi)";
-    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini (GSM)";
-    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3 (GSM)";
-    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
-    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (GSM+CDMA)";
-    if ([platform isEqualToString:@"i386"])         return @"Simulator";
-    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
-    return platform;
-}
+//- (NSString *) platformString{
+//    NSString *platform = [Sparrow.currentController.parentViewController platform];
+//    if ([platform isEqualToString:@"iPhone1,1"])    return @"iPhone 1G";
+//    if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
+//    if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
+//    if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
+//    if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
+//    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
+//    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
+//    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
+//    if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
+//    if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
+//    if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
+//    if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
+//    if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
+//    if ([platform isEqualToString:@"iPad1,1"])      return @"iPad";
+//    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
+//    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
+//    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
+//    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2 (WiFi)";
+//    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini (WiFi)";
+//    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini (GSM)";
+//    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini (GSM+CDMA)";
+//    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3 (WiFi)";
+//    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3 (GSM+CDMA)";
+//    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3 (GSM)";
+//    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
+//    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
+//    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (GSM+CDMA)";
+//    if ([platform isEqualToString:@"i386"])         return @"Simulator";
+//    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
+//    return platform;
+//}
 @end
