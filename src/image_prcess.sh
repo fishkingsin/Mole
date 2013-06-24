3de28d0ba0ba57657f4c44df34210e4bc99027f5
fishkingsin <fishkingsin@gmail.com>
2013-06-24 19:49:03 +0800

    update icon

---
+++ src/image_prcess.sh
@@ -0,0 +1,5 @@
+convert Large\ App\ Icon.png -resize 57x57 icon.png
+convert Large\ App\ Icon.png -resize 114x114 Icon\@2x.png
+convert Large\ App\ Icon.png -resize 144x144 Icon\@2x~ipad.png
+convert Large\ App\ Icon.png -resize 72x72 Icon\~ipad.png
+convert Large\ App\ Icon.png -resize 512x512 iTunesArtwork
